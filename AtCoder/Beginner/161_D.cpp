556e06a15f3b6ef3e164086de83a36744ad9c29b
rikumiura <1810370116h@kindai.ac.jp>
2020-04-17 13:24:25 +0900

    答え見たけど間違った

--- AtCoder/Beginner/161_D.cpp
+++ AtCoder/Beginner/161_D.cpp
@@ -1,6 +1,6 @@
 /**
 *    author:  Dooloper
-*    created: 16.04.2020 03:28:09
+*    created: 17.04.2020 13:15:39
 **/
 
 #include <iostream>
@@ -9,12 +9,31 @@
 #include <string>
 #include <utility>
 #include <algorithm>
+#include <queue>
+#include <deque>
+#include <stack>
+
 
 using namespace std;
 typedef unsigned long long ll;
 
 int main() {
     cin.tie(0);
+    int K;cin>>K;
+    queue<int> Q;
+    for(int i = 1; i < 10; i++){
+        Q.push(i);
+    }
+    int take;
+    for (int i = 0; i < K; i++)
+    {
+        take = Q.front();
+        Q.pop();
+        Q.push(take * 10 + take % 10 - 1);
+        Q.push(take * 10 + take % 10);
+        Q.push(take * 10 + take % 10 +1);
+    }
+    cout<<take<<endl;
     
     return 0;
 }
