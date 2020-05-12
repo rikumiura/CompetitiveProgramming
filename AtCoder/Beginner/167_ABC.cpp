eedc75a8afed0b376f6602a54ca32ad64f253bb6
rikumiura <1810370116h@kindai.ac.jp>
2020-05-12 19:15:47 +0900

    もう少しいいやり方ありそう

--- AtCoder/Beginner/167_ABC.cpp
+++ AtCoder/Beginner/167_ABC.cpp
@@ -19,12 +19,14 @@ typedef unsigned long long ll;
 
 int main() {
     cin.tie(0);
-    string S;cin>>S;
-    string T;cin>>T;
-    S += T.back();
-    if(equal(S.begin(), S.end(), T.begin(),T.end()))
-        cout<<"Yes"<<endl;
-    else 
-        cout<<"No"<<endl;
+    int A,B,C,K;
+    cin>>A>>B>>C>>K;
+    int cnt = 0;
+    int card = K;
+    cnt += min(card,A)*1;
+    card -= min(card,A);
+    card -= min(card,B);
+    cnt += min(card,C)*(-1);
+    cout<<cnt<<endl;
     return 0;
 }
